62fb0d529dbabbf46db3486d5912347ce80b6f76
Aaron Sala <salaaron2@gmail.com>
2023-05-05 20:53:26 +0300

    adding the show number when the game ends

--- prac.js
+++ prac.js
@@ -1,4 +1,5 @@
-let guessNumber= Math.floor(Math.random()* 100) +1;
+let guessNumber= Math.floor(Math.random()* 100)+1 ;
+
 const working = document.querySelector("working")
 const guess = document.querySelector(".guess")
 const range = document.querySelector(".range")
@@ -7,11 +8,12 @@ const range = document.querySelector(".range")
  const number=document.querySelector(".previousguess")
 
 btn.addEventListener("click", clicked)
-
+console.log(guessNumber)
 let guessCount =1;
 let resetButton;
 function clicked(){
    const userGuess = Number(guess.value)
+   
    
    if (guessCount ===1){
     number.textContent = 'Previous guesses: ';
@@ -21,8 +23,8 @@ function clicked(){
     //number.innerHTML= "you guessed "+  guess.value
     
     if(guess.value ==guessNumber){
-        answer.style.background='green';
-        answer.innerHTML="correct you won";
+        answer.style.background='lightgreen';
+        answer.innerHTML="correct you won...";
         range.innerHTML="";
        setGameOver();
       
@@ -30,18 +32,21 @@ function clicked(){
     else if(guessCount === 10){
         answer.innerHTML="!!!Game Over!!!" 
         range.innerHTML="";
+        result=document.createElement('p')
+        result.innerHTML= "the number was " + `${guessNumber}`
+        document.body.append(result);
         setGameOver();
        
     }
     
     else if(guess.value<guessNumber){
-        answer.innerHTML="wrong!!!" 
+        answer.innerHTML="wrong Number!!!" 
         answer.style.background="red"
         range.innerHTML="you have guessed too low"
        
     }
     else {
-        answer.innerHTML="wrong!!!" 
+        answer.innerHTML="wrong Number!!!" 
         answer.style.background="red"
         range.innerHTML="you have guessed too high" 
     }
@@ -66,6 +71,7 @@ function setGameOver() {
     const resetsAll = document.querySelectorAll('.working p');
     for (const resetAll of resetsAll) {
       resetAll.textContent = '';
+      result.textContent="";
     }
   
     resetButton.parentNode.removeChild(resetButton);
